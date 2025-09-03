d3cd17d4e57c7f3d86e4a6fe6a2074c18bbfee96
silence-breaker <weij63@mail2.sysu.edu.cn>
2025-09-03 11:50:04 +0800

    初次提交C程序

---
+++ C/转义字符练习.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");//换行符
+    printf("This is a backslash: \r");//回车符
+    printf("This is a double quote\f");//换页符
+    printf("\tThis is a C17 standard program.");//水平制表符
+    printf("I don not know what to say.\v");//垂直制表符
+    return 0;
+}
