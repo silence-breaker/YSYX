2668135c5248cd617d8cad77927e7aab75ccfbb7
silence-breaker <weij63@mail2.sysu.edu.cn>
2025-09-10 23:50:55 +0800

    Auto update: 2025-09-10 23:50:55

--- C/循环练习.c
+++ C/循环练习.c
@@ -24,6 +24,7 @@ int main(void)
     return 0;
 }
 */
+/*计算1到100中9的倍数的个数
 
 int cout_nine(void)
 {
@@ -44,5 +45,100 @@ int main(void)
 {
     int count = cout_nine();
     printf("1到100有%d个9的倍数\n",count);
+    return 0;
+}
+*/
+/*输出100以内的素数
+int is_prime(int n)
+{
+	int i;
+	for (i = 2; i < n; i++)
+		if (n % i == 0)
+            return 0;
+    return 1;
+}
+
+int main(void)
+{
+	int i;
+	for (i = 2; i <= 100; i++) {
+		if (is_prime(i))
+			printf("%d\n", i);
+	}
+	return 0;
+}
+*/
+
+/*
+int main(void)
+{
+	int i, j;
+	for (i=1; i<=9; i++) {
+		for (j=1; j<=9; j++)
+			printf("%d\t", i*j);
+		printf("\n");
+	}
+	return 0;
+}
+*/
+/*打印小九九
+int main(void)
+{
+    int i,j;
+    for(i=1;i<=9;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d\t",j*i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+*/
+int diamond(int n, char c)
+{
+    int i,j;
+   
+    if(n%2==0)
+    {
+        printf("请输入奇数\n");
+        return -1;
+    };
+    for(i=1;i<=n;i++)
+    {
+        if(i<=n/2+1)
+        {
+            for(j=n/2+1-i;j>0;j--)
+            {
+                printf(" ");
+            }
+            for(j=1;j<=2*i-1;j++)
+            {
+                printf("%c",c);
+            }
+            printf("\n");
+        }
+        else
+        {
+            for(j=1;j<=i-n/2-1;j++)
+            {
+                printf(" ");
+            }
+            for(j=1;j<=2*(n-i)+1;j++)
+            {
+                printf("%c",c);
+            }
+            printf("\n");
+        }
+    }
+};
+int main(void)
+{
+    int n;
+    char c;
+    printf("请输入一个奇数和一个字符：");
+    scanf("%d %c",&n,&c);
+    diamond(n,c);
     return 0;
 }
