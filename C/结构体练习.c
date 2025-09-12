ce6c1161b7392f3e41ac3e7e79bf7fd20baadd04
silence-breaker <weij63@mail2.sysu.edu.cn>
2025-09-12 10:07:46 +0800

    Auto update: 2025-09-12 10:07:46

--- C/结构体练习.c
+++ C/结构体练习.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+/*数据抽象练习
 struct rational{
     int numerator;
     int denominator;
@@ -63,12 +64,84 @@ void print_rational(struct rational r)
 }
 int main(void)
 {
-	struct rational a = make_rational(1, 8); /* a=1/8 */
-	struct rational b = make_rational(-1, 8); /* b=-1/8 */
+	struct rational a = make_rational(1, 8); 
+	struct rational b = make_rational(-1, 8); 
 	print_rational(add_rational(a, b));
 	print_rational(sub_rational(a, b));
 	print_rational(mul_rational(a, b));
 	print_rational(div_rational(a, b));
 
 	return 0;
+}
+*/
+enum coordinate_type{RECTANGULAR, POLAR};
+
+struct complex_struct{
+    enum coordinate_type t;
+    double a,b;
+};
+struct complex_struct make_from_real_img(double x, double y)
+{
+	struct complex_struct z;
+	z.t = RECTANGULAR;
+	z.a = x;
+	z.b = y;
+	return z;
+}
+
+struct complex_struct make_from_mag_ang(double r, double A)
+{
+	struct complex_struct z;
+	z.t = POLAR;
+	z.a = r;
+	z.b = A;
+	return z;
+}
+
+double real_part(struct complex_struct z)
+{
+    if(z.t== RECTANGULAR)
+    {
+        return z.a;
+    }
+    else
+    {
+        return z.a * cos(z.b);
+    }
+}
+
+double imag_part(struct complex_struct z)
+{
+    if(z.t== RECTANGULAR)
+    {
+        return z.b;
+    }
+    else
+    {
+        return z.a * sin(z.b);
+    }
+}
+
+double magnitude(struct complex_struct z)
+{
+    if(z.t== RECTANGULAR)
+    {
+        return sqrt(z.a*z.a + z.b*z.b);
+    }
+    else
+    {
+        return z.a;
+    }
+}
+
+double angle(struct complex_struct z)
+{
+    if(z.t== RECTANGULAR)
+    {
+        return atan2(z.b, z.a);
+    }
+    else
+    {
+        return z.b;
+    }
 }
