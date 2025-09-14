b509a53b3bd3c9b894b128c1586a9d6b0ea13f08
silence-breaker <weij63@mail2.sysu.edu.cn>
2025-09-15 00:14:31 +0800

    Auto update: 2025-09-15 00:14:31

--- C/数组练习.c
+++ C/数组练习.c
@@ -92,26 +92,26 @@ int main()
 
 int main(void)
 {
-	char gesture[3][10] = {"scissor","stone","cloth"};
-	int man,computer,result,ret;
-	srand(time(NULL));
-	while(1){
-		computer = rand()%3;
-		printf("\nInput your gesture (0-scissor 1-stone 2-cloth):\n ");
-		ret = scanf("%d",&man);
-		if(ret!=1 || man<0 || man>2)
-		{
-			printf("Invalid input! Please input 0,1 or 2.\n");
-			continue;
-		}
-		printf("Your gesture: %s\t Computer's gesture: %s\n",gesture[man],gesture[computer]);
-		result = (man-computer + 4)%3-1;
-		if(result==0)
-			printf("It's a draw!\n");
-		else if(result==1)
-			printf("You win!\n");
-		else
-			printf("You lose!\n");
+    char gesture[3][10] = { "scissor", "stone", "cloth" };
+    int man, computer, result, ret;
+    srand(time(NULL));
+    while (1) {
+	computer = rand() % 3;
+	printf("\nInput your gesture (0-scissor 1-stone 2-cloth):\n ");
+	ret = scanf("%d", &man);
+	if (ret != 1 || man < 0 || man > 2) {
+	    printf("Invalid input! Please input 0,1 or 2.\n");
+	    continue;
 	}
-	return 0;
+	printf("Your gesture: %s\t Computer's gesture: %s\n", gesture[man],
+	       gesture[computer]);
+	result = (man - computer + 4) % 3 - 1;
+	if (result == 0)
+	    printf("It's a draw!\n");
+	else if (result == 1)
+	    printf("You win!\n");
+	else
+	    printf("You lose!\n");
+    }
+    return 0;
 }
