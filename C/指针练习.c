05c4e7a0c49032b8785ae36b6ab8e9b62dc7eef4
silence-breaker <weij63@mail2.sysu.edu.cn>
2025-09-22 23:15:37 +0800

    Auto update: 2025-09-22 23:15:37

--- C/指针练习.c
+++ C/指针练习.c
@@ -13,7 +13,7 @@ char show_ppa; // 声明为全局变量，但不在这里初始化
 int main(void)
 {
     // 在函数内部初始化
-    show_pa = (*(pa+5))[1];
+    show_pa = (*(pa+5))[1]; //不像数组名，可以直接加索引，需要先解引用为数组然后再加索引
     show_ppa = (*(ppa+1))[2][1];
     
     printf("%c %c\n", show_pa, show_ppa);
