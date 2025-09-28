b18a69aefc5d86dcd51cdbd25133c6327a5d1d49
silence-breaker <weij63@mail2.sysu.edu.cn>
2025-09-28 20:40:13 +0800

    Auto update: 2025-09-28 20:40:13

--- C/函数接口练习.c
+++ C/函数接口练习.c
@@ -66,6 +66,7 @@ int main(void)
     return 0;
 }
 */
+/*
 int main(void)
 {
     int i = 1;
@@ -74,4 +75,213 @@ int main(void)
         int *p = malloc(i);
         i = 2*i;
     }
+}
+*/
+/*
+int add(int a)
+{
+    a = a+1;
+    return a;
+}
+
+int main(void)
+{
+    int a = 0;
+    add(a);
+    printf("%d\n",a);
+    return 0;
+
+}
+*/
+/*
+#include <stdarg.h>
+
+// 辅助函数：输出整数
+void print_int(int num) {
+    if (num < 0) {
+        putchar('-');
+        num = -num;
+    }
+    if (num >= 10) {
+        print_int(num / 10);
+    }
+    putchar(num % 10 + '0');
+}
+
+// 辅助函数：输出浮点数
+void print_float(double num) {
+    if (num < 0) {
+        putchar('-');
+        num = -num;
+    }
+    
+    // 输出整数部分
+    int int_part = (int)num;
+    print_int(int_part);
+    
+    // 输出小数点
+    putchar('.');
+    
+    // 输出小数部分（保留6位）
+    double frac_part = num - int_part;
+    for (int i = 0; i < 6; i++) {
+        frac_part *= 10;
+        int digit = (int)frac_part;
+        putchar(digit + '0');
+        frac_part -= digit;
+    }
+}
+
+// 辅助函数：输出字符串
+void print_string(const char *str) {
+    while (*str) {
+        putchar(*str);
+        str++;
+    }
+}
+
+void myprintf(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    
+    while (*format) {
+        if (*format == '%') {
+            format++;
+            if (*format == 'd') {
+                int i = va_arg(args, int);
+                print_int(i);  // 修改：使用专门的函数输出整数
+            } else if (*format == 'f') {
+                double d = va_arg(args, double);
+                print_float(d);  // 修改：使用专门的函数输出浮点数
+            } else if (*format == 'c') {
+                // char 被提升为 int
+                int c = va_arg(args, int);
+                putchar(c);
+            } else if (*format == 's') {
+                char *s = va_arg(args, char *);
+                print_string(s);  // 修改：使用专门的函数输出字符串
+            } else {
+                putchar('%');
+                putchar(*format);
+            }
+        } else {
+            putchar(*format);
+        }
+        format++;
+    }
+    
+    va_end(args);
+}
+
+int main(void)
+{
+    myprintf("Hello %s! Your score is %d and average is %f. Grade: %c\n", "Alice", 95, 89.5, 'A');
+    return 0;
+}
+*/
+
+#include <string.h>
+
+char *strtok(char *str, const char *delim)
+{
+    static char *last = NULL; // 保存上次调用的位置
+    if (str != NULL) {
+        last = str; // 如果传入新字符串，重置 last
+    } else if (last == NULL) {
+        return NULL; // 如果没有更多的字符串可处理，返回 NULL
+    }
+
+    // 跳过开头的分隔符
+    while (*last && strchr(delim, *last)) {
+        last++;
+    }
+
+    if (*last == '\0') {
+        last = NULL; // 没有更多的标记
+        return NULL;
+    }
+
+    char *start = last; // 标记当前 token 的开始位置
+
+    // 找到下一个分隔符
+    while (*last && !strchr(delim, *last)) {
+        last++;
+    }
+
+    if (*last) {
+        *last = '\0'; // 用 null 字符终止当前 token
+        last++; // 移动到下一个字符，准备下一次调用
+    } else {
+        last = NULL; // 没有更多的标记
+    }
+
+    return start; // 返回当前 token 的起始位置
+
+}
+
+char *strtok_r(char *str, const char *delim, char **saveptr)
+{
+    char *start;
+
+    if (str != NULL) {
+        *saveptr = str; // 如果传入新字符串，重置 saveptr
+    } else if (*saveptr == NULL) {
+        return NULL; // 如果没有更多的字符串可处理，返回 NULL
+    }
+
+    // 跳过开头的分隔符
+    while (**saveptr && strchr(delim, **saveptr)) {
+        (*saveptr)++;
+    }
+
+    if (**saveptr == '\0') {
+        *saveptr = NULL; // 没有更多的标记
+        return NULL;
+    }
+
+    start = *saveptr; // 标记当前 token 的开始位置
+
+    // 找到下一个分隔符
+    while (**saveptr && !strchr(delim, **saveptr)) {
+        (*saveptr)++;
+    }
+
+    if (**saveptr) {
+        **saveptr = '\0'; // 用 null 字符终止当前 token
+        (*saveptr)++; // 移动到下一个字符，准备下一次调用
+    } else {
+        *saveptr = NULL; // 没有更多的标记
+    }
+
+    return start; // 返回当前 token 的起始位置
+}
+
+char URL[]="http://www.google.cn/search?complete=1&hl=zh-CN&ie=GB2312&q=linux&meta=";
+
+int main(void)
+{
+    printf("%s\n",URL);
+    
+    char *token;
+    printf("使用 strtok:\n");
+    token = strtok(URL,"?");
+    printf("路径部分为\n");
+    printf("%s\n",token);
+    printf("键部分为\n"); 
+    while ((token = strtok(NULL, "&")) != NULL) {
+        printf("%s\n",token);
+    }
+    //注意在执行strtok的时候已经把URL数组的特殊字符都改成了'\0'，所以如果再用strtok的话就得重新赋值URL数组
+    strcpy(URL,"http://www.google.cn/search?complete=1&hl=zh-CN&ie=GB2312&q=linux&meta=");
+    printf("使用 strtok_r:\n");
+    char *saveptr;
+    token = strtok_r(URL,"?",&saveptr);
+    printf("路径部分为\n");
+    printf("%s\n",token);
+    printf("键部分为\n"); 
+    while ((token = strtok_r(NULL, "&",&saveptr)) != NULL) {
+        printf("%s\n",token);
+    }
+    return 0;
 }
